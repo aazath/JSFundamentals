8a5438c9fdd706a080d5be1e6b3f06752c026cb4
aazath <asaazath@gmail.com>
2022-11-04 14:44:40 +0530

    "Authenticated"

--- SignOut.js
+++ SignOut.js
@@ -1,10 +1,14 @@
 //Show user a signout button if he is authenticated
 //otherwise show him option to Login/Signup
 
-var authenticated = true;
+var authenticated = false;
 
-if (authenticated) {
-    console.log("show signout button");
-}else{
-    console.log("show login/signup button");
-}
+//tradional way of doing 
+// if (authenticated) {
+//     console.log("show signout button");
+// }else{
+//     console.log("show login/signup button");
+// }
+
+//using the ternary operator
+(authenticated ? console.log("Show signout button") : console.log("Show login/signup button" ) );
