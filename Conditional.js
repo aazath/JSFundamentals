e539334824def78e69dcc7937a623c1ea71e402e
aazath <asaazath@gmail.com>
2022-11-04 14:19:40 +0530

    Conditionals in JS

---
+++ Conditional.js
@@ -0,0 +1,11 @@
+var temperature;
+
+//TODO: go to google and get the data
+temperature = 19;
+
+if(temperature < 20){
+    console.log("It's very cold outside");
+}
+else{
+    console.log("It's fire outside");
+}
