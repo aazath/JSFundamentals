2a9e5a12451fa1e9dcc552b3f48a145e97b0ea4a
aazath <asaazath@gmail.com>
2022-11-04 14:39:51 +0530

    Login

---
+++ ConditionalLogin.js
@@ -0,0 +1,12 @@
+//Allow user to access the page if:
+//logged in from google
+//logged in from email
+//logged in from facebook
+
+var email = true;
+var facebook = false;
+var google = false;
+
+if (email || faceboo || google) {
+    console.log("Login success");
+}
---
+++ SignOut.js
@@ -0,0 +1,10 @@
+//Show user a signout button if he is authenticated
+//otherwise show him option to Login/Signup
+
+var authenticated = true;
+
+if (authenticated) {
+    console.log("show signout button");
+}else{
+    console.log("show login/signup button");
+}
